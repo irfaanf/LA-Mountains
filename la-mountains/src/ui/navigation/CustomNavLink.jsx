3d525707711fad327d374bd5e42bd344e483c9ea
Irfaan Fakir <irfaan@hotmail.co.za>
2024-03-10 16:38:49 +0200

    Deleted for rename.

--- la-mountains/src/ui/navigation/CustomNavLink.jsx
+++
@@ -1,9 +0,0 @@
-import Nav from 'react-bootstrap/Nav';
-
-const CustomNavLink = ({ children }) => {
-    return (
-        <Nav.Link className='text-white'>{children}</Nav.Link>
-    );
-}
-
-export default CustomNavLink;
